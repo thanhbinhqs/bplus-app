233744a739ddbd8f8a2dca907fdbe3777b9cfd34
bplus <thanhbinhqs@gmail.com>
2025-05-13 05:34:28 +0700

    change page logo link

--- app/components/app/common/app-sidebar.tsx
+++ app/components/app/common/app-sidebar.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { Link } from "@remix-run/react";
 import {
   Sidebar,
   SidebarContent,
@@ -27,10 +28,10 @@ export default function AppSidebar({ ...props }) {
           <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
             <img src="/images/plus.png" alt="Logo" className="size-4" />
           </div>
-          <div className="grid flex-1 text-left text-sm leading-tight">
+          <Link to="/" className="grid flex-1 text-left text-sm leading-tight">
             <span className="truncate font-semibold">Bplus</span>
             <span className="truncate text-xs">Go to future</span>
-          </div>
+          </Link>
         </SidebarMenuButton>
       </SidebarHeader>
       <SidebarContent>
